a382076d7fb8f197fc5ccdf860b2ff94feba3419
Dante Monaldo <dante.monaldo@gmail.com>
2018-05-11 15:02:31 -0700

    Removed worker example function

--- main.cpp
+++ main.cpp
@@ -55,20 +55,6 @@ int pthread_sleep (int seconds) {
 }
 
 /******************************************************************************
-* This is an example function that becomes a thread. It takes a pointer
-* parameter so we could pass in an array or structure.
-*****************************************************************************/
-void *worker(void *arg) {
-  while(1) {
-    printf("Thread Running\n");
-    fflush(stdout);
-    pthread_sleep(1);
-  }
-
-  return NULL;
-}
-
-/******************************************************************************
 * The main function is just an infinite loop that spawns off a second thread
 * that also is an infinite loop. We should see two messages from the worker
 * for every one from main.
@@ -77,30 +63,24 @@ int main() {
   pthread_t t_id;
   int pshared = 0;
   int value = 1;
-  // queue<car> nReadyQ;
-  // queue<car> sReadyQ;
 
-  if (pthread_mutex_init(&flagPersonMutex,NULL)) {
+  if (pthread_mutex_init(&flagPersonMutex, NULL)) {
     return -1;
   }
-  if (pthread_cond_init(&flagPersonCondition,NULL)) {
+  if (pthread_cond_init(&flagPersonCondition, NULL)) {
     return -1;
   }
 
-  if(0 != sem_open("mySem", carSem, pshared, value)) //value is 1 because this is a lock
+  if (0 != sem_open("mySem", carSem, pshared, value)) {
+    //value is 1 because this is a lock
     perror("sem_init");
     return -1;
-  if ( -1 == pthread_create(&t_id, NULL, worker, NULL) ) {
-    perror("pthread_create");
-    return -1;
   }
 
-  while(1) {
+  while (1) {
     printf("Main Running\n");
-    sem_wait(&carSem);
-    //
     fflush(stdout);
-    pthread_sleep(2);
+    pthread_sleep(1);
   }
 
   sem_close(&carSem);
